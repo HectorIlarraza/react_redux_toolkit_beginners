ac65feacdab3dcbbeb99d1872087d9c881cc163f
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-11-02 08:12:14 -0500

    completed lesson 2 Redux for beginners

--- lesson_02/src/app/store.js
+++ lesson_02/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from "../features/postS/postsSlice";
+import postsReducer from "../features/posts/postsSlice";
+import usersSlice from "../features/users/usersSlice";
+
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersSlice
   },
 });
